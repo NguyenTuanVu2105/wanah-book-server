e2158342a3ea9939f25671ed3a2d304c98f1cd97
NguyenTuanVu2105 <daigiachantran01@gmail.com>
2019-10-30 22:07:29 +0700

    Fix cors

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
+const cors = require('cors')
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 require('dotenv').config();
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -29,9 +31,9 @@ require('./config/passport')(passport);
 app.use('/api', users);
 app.use('/api', profile);
 
-// app.use('/', (req, res) => {
-//     res.send("Hello world!!");
-// });
+app.use('/api', (req, res) => {
+    res.send("Hello world!!");
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
