0c7ddb82315c27616faedb69b90b3e49000f28ae
NguyenTuanVu2105 <daigiachantran01@gmail.com>
2019-11-19 01:57:52 +0700

    fix deploy

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -12,7 +12,15 @@ const Sequelize = require('sequelize');
 //     }
 // });
  
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config.json')[env];
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 const db = {};
  
