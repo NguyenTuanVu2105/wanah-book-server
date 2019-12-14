2aea1fc95325e1b97828af6a5359856110737a6d
NguyenTuanVu2105 <daigiachantran01@gmail.com>
2019-12-14 08:04:57 +0700

    fix profile

--- server/controller/profile.controller.js
+++ server/controller/profile.controller.js
@@ -1,6 +1,7 @@
 const db = require('../config/db.config');
 const User = db.user;
 const Profile = db.profile;
+const Category = db.category;
 const Op = db.Sequelize.Op;
 //sửa profile //complete
 exports.editProfile = (req, res) => {
@@ -23,9 +24,32 @@ exports.Profile = (req, res) => {
         }
     }).then( profile => {
         if(profile) {
-            res.status(200).send({first_name : profile.first_name,last_name:profile.last_name, })
+            Category.findAll({
+                include: {
+                    model: Profile,
+                    through: {
+                        attributes: ['name'],
+                        where: {profileId: profile.id}
+                    }
+                }
+            }).then( category => {
+                res.status(200).send({
+                    first_name : profile.first_name,
+                    last_name  : profile.last_name,
+                    description: profile.description,
+                    address_detail: profile.address_detail,
+                    category: category.map(x => {
+                        return {
+                            id: x.id,
+                            name: x.name
+                        }
+                    })
+                 })
+            }).catch(err => {
+                res.status(500).send({message: err})
+            })
         }
-    }).catch(error => {
+    }).catch(err => {
         res.status(500).send({message: err})
     })
 }
