2234067a2a982318d971c80101dd6dc26bdae15b
lyminhnghia <lmn2461999@gmail.com>
2019-12-17 11:28:57 +0700

    update api get reviewbyBook and reviewbyUser

--- server/controller/review.controller.js
+++ server/controller/review.controller.js
@@ -10,14 +10,19 @@ exports.addReview = (req, res) => {
         star: req.body.star,
         userId: req.userId,
         bookId: req.body.bookId
-    }).then(() => {
+    }).then(review => {
+        Vote.create({
+            userId: req.userId,
+            reviewId: review.id,
+            is_upvote: 0
+        })
         res.status(200).send({success: true});
     }).catch(err => res.status(500).send(err.message));
 
-    db.sequelize.query('CREATE TRIGGER review_trg ON reviews FOR EACH ROW' +
-    ' BEGIN' +
-    ' update books set star = 3 where id = 1;' +
-    'END;')
+    // db.sequelize.query('CREATE TRIGGER review_trg ON reviews FOR EACH ROW' +
+    // ' BEGIN' +
+    // ' update books set star = 3 where id = 1;' +
+    // 'END;')
 }
 
 exports.addVote = (req, res) => {
@@ -74,6 +79,7 @@ exports.goodReview = (req, res) => {
 }
 
 exports.reviewByBook = (req, res) => {
+    var arrResult = [];
     Book.findOne({
         where: {
             id: req.query.id
@@ -84,16 +90,33 @@ exports.reviewByBook = (req, res) => {
             attributes: ['id', 'content', 'star', 'bookId'],
             include: [{
                 model: Vote,
-                attributes: ['reviewId', [db.sequelize.fn('COUNT', db.sequelize.col('is_upvote')), 'count']],
-                group: ['books.id']
+                attributes: ['id', 'is_upvote', 'userId', 'reviewId']
             }],
         }],
     }).then(AllInfor => {
-        res.status(200).send(AllInfor);
+        for (var i = 0 ; i < AllInfor.reviews.length; i++) {
+            let data = {};
+            if (AllInfor.reviews[i].votes.length > 0) {
+                data.reviewId = AllInfor.reviews[i].id;
+                data.count = 0;
+                for (let j = 0 ; j < AllInfor.reviews[i].votes.length ; j++) {
+                    if (AllInfor.reviews[i].votes[j].is_upvote) {
+                        data.count++;
+                    }
+                }
+            } else {
+                data.reviewId = AllInfor.reviews[i].id;
+                data.count = 0;
+            }
+            arrResult.push(data);
+        }
+
+        res.status(200).send({bookInfor: AllInfor, countInfor :arrResult});
     }).catch(err => res.status(500).send({message: err}))
 }
 
 exports.reviewByUser = (req, res) => {
+    var arrResult = [];
     User.findOne({
         where: {
             id: req.query.id
@@ -105,12 +128,28 @@ exports.reviewByUser = (req, res) => {
             attributes: ['id', 'content', 'star', 'bookId'],
             include: [{
                 model: Vote,
-                attributes: ['reviewId',[db.sequelize.fn('COUNT', db.sequelize.col('is_upvote')), 'count']],
-                group: ['users.id']
+                attributes: ['id', 'is_upvote', 'userId', 'reviewId']
             }]
         }]
     }).then(AllInfor => {
-        res.status(200).send(AllInfor);
+        for (var i = 0 ; i < AllInfor.reviews.length; i++) {
+            let data = {};
+            if (AllInfor.reviews[i].votes.length > 0) {
+                data.reviewId = AllInfor.reviews[i].id;
+                data.count = 0;
+                for (let j = 0 ; j < AllInfor.reviews[i].votes.length ; j++) {
+                    if (AllInfor.reviews[i].votes[j].is_upvote) {
+                        data.count++;
+                    }
+                }
+            } else {
+                data.reviewId = AllInfor.reviews[i].id;
+                data.count = 0;
+            }
+            arrResult.push(data);
+        }
+
+        res.status(200).send({userInfor: AllInfor, countInfor :arrResult});
     }).catch(err => res.status(500).send({message: err}))
 }
 
